5919fba7829961e53f3f7a0c525477ccb1699fcb
Keir Stiegler <keir.stiegler@gmail.com>
2024-01-31 17:20:40 -0600

    Update speed ranges for Jasco model 14314
    Following what was done previously for similar fans (https://github.com/home-assistant/core/pull/60517), this change matches the speed settings for low, medium, and high, which is all this fan supports.

--- homeassistant/components/zwave_js/discovery.py
+++ homeassistant/components/zwave_js/discovery.py
@@ -297,10 +297,14 @@ DISCOVERY_SCHEMAS = [
     # GE/Jasco - In-Wall Smart Fan Control - 14314 / ZW4002
     ZWaveDiscoverySchema(
         platform=Platform.FAN,
+        hint="has_fan_value_mapping",
         manufacturer_id={0x0063},
         product_id={0x3138},
         product_type={0x4944},
         primary_value=SWITCH_MULTILEVEL_CURRENT_VALUE_SCHEMA,
+        data_template=FixedFanValueMappingDataTemplate(
+            FanValueMapping(speeds=[(1, 32), (33, 66), (67, 99)]),
+        ),
     ),
     # Leviton ZW4SF fan controllers using switch multilevel CC
     ZWaveDiscoverySchema(
